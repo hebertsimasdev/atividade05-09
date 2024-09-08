1f5ec142976190dd656a7363be81456d926ef871
hebertsimasdev <hebertt.simas@hotmail.com>
2024-09-08 18:07:50 -0300

    rewiwAtividade06.php

--- atividade06.php
+++ atividade06.php
@@ -7,8 +7,26 @@
 </head>
 <body>
     <?php
-
-    ?>
     
+$valor=$_POST["n1"];
+$dinheiro='0';
+
+if ($valor=='1') {
+    echo"Seu saldo é de $dinheiro";
+    
+}elseif ($valor=='2') {
+    echo"INFORME O VALOR A SER SACADO";
+    
+
+}elseif ($valor=='3') {
+    echo"INFORME O VALOR A SER DEPOSITADO";
+
+}
+elseif ($valor=='4') {
+    echo"Até logo...";
+}
+else {
+echo"Digite uma opção válida";}
+    ?>
 </body>
 </html>
